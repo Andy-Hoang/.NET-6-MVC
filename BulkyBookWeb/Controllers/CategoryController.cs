1a549bbb51f153bb01100557b69debfa9135b482
andy.hoang <andy.hoang@area9.com.au>
2023-06-20 17:15:20 +0930

    feat: add Delete and _Notification partial view using TempData

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -40,6 +40,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();      //actual action made to Database
+                TempData["Success"] = "Category created successfully"; 
                 return RedirectToAction("Index");       // if no Controller name, then default is the current Controller
             }
             return View(obj);       //pass the current 'obj' to see the current input values caused error
@@ -73,14 +74,49 @@ namespace BulkyBookWeb.Controllers
                 //key 'name' will bind to show error inside 'Name' prop of Category
                 ModelState.TryAddModelError("name", "DisplayOrder cannot exactly match the Name.");
             }
-            if (ModelState.IsValid)     //server side validation
+            if (ModelState.IsValid)
             {
                 _db.Categories.Update(obj);     //Entity Framework does all the work: find the 'obj' by id in DB, look for what props changed, and update
-                _db.SaveChanges();      //actual action made to Database
-                return RedirectToAction("Index");       // if no Controller name, then default is the current Controller
+                _db.SaveChanges();
+                TempData["Success"] = "Category edited successfully";
+                return RedirectToAction("Index");       
             }
-            return View(obj);       //pass the current 'obj' to see the current input values caused error
+            return View(obj);       
 
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDB = _db.Categories.Find(id);
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDB);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var categoryFromDB = _db.Categories.Find(id);
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(categoryFromDB);     //Entity Framework does all the work: find the 'obj' by id in DB, look for what props changed, and update
+            _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
